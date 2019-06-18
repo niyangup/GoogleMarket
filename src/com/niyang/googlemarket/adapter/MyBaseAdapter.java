c5b4d24823c8b6d67cb5e7dcb2da780b5b58da8b
niyangup <niyangup@163.com>
2019-06-19 07:46:56 +0800

    代码重构

---
+++ src/com/niyang/googlemarket/adapter/MyBaseAdapter.java
@@ -0,0 +1,41 @@
+package com.niyang.googlemarket.adapter;
+
+import java.util.ArrayList;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**对Adapter的封装
+ * @author niyang
+ *
+ */
+public class MyBaseAdapter<T> extends BaseAdapter{
+	
+	private ArrayList<T> data;
+
+	public MyBaseAdapter(ArrayList<T> data) {
+		this.data=data;
+	}
+	
+	@Override
+	public int getCount() {
+		return data.size();
+	}
+
+	@Override
+	public T getItem(int position) {
+		return data.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		return null;
+	}
+
+}
--- src/com/niyang/googlemarket/fragment/HomeFragment.java
+++ src/com/niyang/googlemarket/fragment/HomeFragment.java
@@ -1,29 +1,77 @@
 package com.niyang.googlemarket.fragment;
 
+import java.util.ArrayList;
+
+import com.niyang.googlemarket.R;
+import com.niyang.googlemarket.adapter.MyBaseAdapter;
 import com.niyang.googlemarket.utils.UIUtils;
 import com.niyang.googlemarket.view.LoadingPage.ResultState;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-/**首页
+/**
+ * 首页
+ * 
  * @author niyang
  *
  */
-public class HomeFragment extends BaseFragment{
+public class HomeFragment extends BaseFragment {
 
-	//如果加载成功,就回调此方法,在主线程运行
+	private ArrayList<String> data;
+
+	// 如果加载成功,就回调此方法,在主线程运行
 	@Override
 	public View onCreateSuccessView() {
-		TextView view = new TextView(UIUtils.getContext());
-		view.setText(getClass().getSimpleName());
+		// TextView view = new TextView(UIUtils.getContext());
+		// view.setText(getClass().getSimpleName());
+
+		ListView view = new ListView(UIUtils.getContext());
+		view.setAdapter(new HomeAdapter(data));
 		return view;
 	}
 
-	//运行在子线程,可以直接执行耗时网络操作
+	// 运行在子线程,可以直接执行耗时网络操作
 	@Override
 	public ResultState onLoad() {
+		data = new ArrayList<String>();
+		for (int i = 0; i < 20; i++) {
+			data.add("测试数据:" + i);
+		}
+
 		return ResultState.STATE_SUCESS;
 	}
 
+	class HomeAdapter extends MyBaseAdapter<String> {
+
+		public HomeAdapter(ArrayList<String> data) {
+			super(data);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder viewHolder = null;
+			if (convertView == null) {
+				viewHolder = new ViewHolder();
+
+				convertView = UIUtils.inflate(R.layout.list_item_home);
+				viewHolder.content = (TextView) convertView.findViewById(R.id.tv_content);
+				convertView.setTag(viewHolder);
+			} else {
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+
+			String content = getItem(position);
+			viewHolder.content.setText(content);
+			return convertView;
+		}
+
+	}
+
+	static class ViewHolder {
+		public TextView content;
+	}
 }
